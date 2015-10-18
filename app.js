5e5c8653a00bbcc02aa888f11ac978702159a367
ghirlekar <gaurav.hirlekar@gmail.com>
2015-10-18 00:23:11 -0400

    Restructured app into separated callback functions. Also connected euler angle data from serial to pitch and roll commands to quad

--- app.js
+++ app.js
@@ -1,77 +1,124 @@
 var RollingSpider = new require('rolling-spider');
+var serialport = require('serialport');
+var hid = require('node-hid');
+
 var quad = new RollingSpider({
     logger: console.log
 });
-var hid = require('node-hid');
+
 // console.log(hid.devices());
 // Logitech Extreme 3D Pro's vendorID and productID: 1133:49685 (i.e. 046d:c215)
 var joystick = new hid.HID(1133, 49685);
-// console.log(joystick);
-
 var flying = false;
 var inFlip = false;
 var prevControls;
 
-quad.connect(function(err) {
-    if (err) console.log(err);
-    quad.setup(function(err) {
-        if (err) console.log(err);
+
+var Serial = serialport.SerialPort(process.argv[2], {
+    baudrate: 115200,
+    parser: serialport.parsers.readline('\n');
+}, true, onSerialConnected);
+
+function onSerialConnected(err) {
+    if (err) console.error(err);
+    else {
+        Serial.flush();
+        quad.connect(onQuadConnected);
+    }
+};
+
+function onQuadConnected(err) {
+    if (err) console.error(err);
+    else {
+        quad.setup(onQuadSetup);
+    }
+}
+
+function onQuadSetup(err) {
+    if (err) console.error(err);
+    else {
         quad.flatTrim();
         quad.startPing();
         quad.flatTrim();
 
-        joystick.on('data', function(buf) {
-            var controls = parseControls(buf);
-            if (!prevControls)
-                prevControls = controls;
-            if (prevControls.buttons[0] === 0 && controls.buttons[0] === 1) {
-                if (flying) {
-                    quad.land();
-                    flying = false;
-                } else {
-                    quad.flatTrim();
-                    quad.takeoff();
-                    flying = true;
-                }
-            }
-            if (prevControls.view === 8 && controls.view !== 8)
-                switch (controls.view) {
-                    case 0:
-                        inFlip = true;
-                        quad.frontFlip(flipDone);
-                        break;
-                    case 2:
-                        inFlip = true;
-                        quad.rightFlip(flipDone);
-                        break;
-                    case 4:
-                        inFlip = true;
-                        quad.backFlip(flipDone);
-                        break;
-                    case 6:
-                        inFlip = true;
-                        quad.leftFlip(flipDone);
-                        break;
-                }
-            prevControls = controls;
-            if (flying && !inFlip) {
-                if (controls.buttons[1]) {
-                    quad.driveStepsRemaining = 1;
-                    quad.speeds = {
-                        yaw: 100 * (controls.yaw - 128) / 128,
-                        pitch: -100 * (controls.pitch - 512) / 512,
-                        roll: 100 * (controls.roll - 512) / 512,
-                        altitude: 100 * (controls.throttle - 128) / 128
-                    }
-                } else quad.hover();
-            }
-            // console.log(JSON.stringify(controls));
-        });
-        joystick.on('error', function() {
+        joystick.on('data', onJoystickData);
+        joystick.on('error', onJoystickError);
+
+        Serial.on('data', onSerialData);
+        Serial.on('error', onSerialError);
+    }
+}
+
+function onJoystickError(err) {
+    console.error(err);
+    if (flying) quad.land();
+}
+
+function onJoystickData(data) {
+    var controls = parseControls(data);
+    if (!prevControls) prevControls = controls;
+    if (prevControls.buttons[0] === 0 && controls.buttons[0] === 1) {
+        if (flying) {
             quad.land();
-        });
-    });
-});
+            flying = false;
+        } else {
+            quad.flatTrim();
+            quad.takeoff();
+            flying = true;
+        }
+    }
+    // if (prevControls.view === 8 && controls.view !== 8)
+    //     switch (controls.view) {
+    //         case 0:
+    //             inFlip = true;
+    //             quad.frontFlip(flipDone);
+    //             break;
+    //         case 2:
+    //             inFlip = true;
+    //             quad.rightFlip(flipDone);
+    //             break;
+    //         case 4:
+    //             inFlip = true;
+    //             quad.backFlip(flipDone);
+    //             break;
+    //         case 6:
+    //             inFlip = true;
+    //             quad.leftFlip(flipDone);
+    //             break;
+    //     }
+    // prevControls = controls;
+    // if (flying && !inFlip) {
+    //     if (controls.buttons[1]) {
+    //         quad.driveStepsRemaining = 1;
+    //         quad.speeds = {
+    //             yaw: 100 * (controls.yaw - 128) / 128,
+    //             pitch: -100 * (controls.pitch - 512) / 512,
+    //             roll: 100 * (controls.roll - 512) / 512,
+    //             altitude: 100 * (controls.throttle - 128) / 128
+    //         }
+    //     } else quad.hover();
+    // }
+
+    // console.log(JSON.stringify(controls));
+}
+
+function onSerialData(data) {
+    var eulerAngles = data.split(',').slice(1);
+    if (flying && !inFlip) {
+        quad.driveStepsRemaining = 1;
+        quad.speeds = {
+            yaw: 0,
+            pitch: -eulerAngles[0],
+            roll: eulerAngles[1],
+            altitude: 0
+        }
+    }
+}
+
+function onSerialError(err) {
+    console.error(err);
+    if (flying) quad.land();
+}
 
 function flipDone() {
     inFlip = false;
